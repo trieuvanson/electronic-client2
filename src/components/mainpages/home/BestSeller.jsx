0c827fd68cbd7cfc1d02a577458f9c97cd191277
Tân Dai <hoangdai12022001@gmail.com>
2021-11-24 12:43:18 +0700

    slider

--- src/components/mainpages/home/BestSeller.jsx
+++ src/components/mainpages/home/BestSeller.jsx
@@ -17,65 +17,55 @@ const Promotion = (props) => {
                 <div className="section-header">
                     <h2>Sản phẩm bán chạy</h2>
                 </div>
-                <div className="row" id="latest-products">
-                    {
-                        item && item.map((item, index) => (
-                            <div className="col-3 col-md-6 col-sm-12" key={item.id}>
-                                <div className="product-card">
-                                    <div className="product-card-img">
-                                        <img src={item.thumbnail} alt=""/>
-                                        <img src={item.thumbnail2} alt=""/>
-                                    </div>
-                                    <div className="product-card-info">
-                                        <div className="product-btn">
-                                            <Link to={`/product/detail/${item.id}`} className="btn-flat btn-hover btn-shop-now">shop now</Link>
-                                            <button className="btn-flat btn-hover btn-cart-add">
-                                                <i className='ti-shopping-cart'/>
-                                            </button>
-                                            <button className="btn-flat btn-hover btn-cart-add">
-                                                <i className='ti-heart'/>
-                                            </button>
-                                        </div>
-                                        <div className="product-card-name">
-                                            {item.name}
-                                        </div>
-                                        <div className="product-card-price">
-                                            <span><del>{formatCash(item.regular_price)} <sup>đ</sup></del></span>
-                                            <span className="curr-price">{formatCash(item.sale_price)} <sup>đ</sup></span>
-                                        </div>
-                                    </div>
+                <div className="row">
+                    <div className="col-12 col-md-12 col-sm-12">
+                        <div className="row">
+                            <div className="glider-contain">
+                                <div className="glider">
+                                    {
+                                        item && item.map((item, index) => (
+                                            <div className="col-4 col-md-6 col-sm-12" key={item.id}>
+                                                <div className="product-card">
+                                                    <div className="product-card-img">
+                                                        <img src={item.thumbnail} alt=""/>
+                                                        <img src={item.thumbnail2} alt=""/>
+                                                    </div>
+                                                    <div className="product-card-info">
+                                                        <div className="product-btn">
+                                                            <Link to={`/product/detail/${item.id}`} className="btn-flat btn-hover btn-shop-now">shop now</Link>
+                                                            <button className="btn-flat btn-hover btn-cart-add">
+                                                                <i className='ti-shopping-cart'/>
+                                                            </button>
+                                                            <button className="btn-flat btn-hover btn-cart-add">
+                                                                <i className='ti-heart'/>
+                                                            </button>
+                                                        </div>
+                                                        <div className="product-card-name">
+                                                            {item.name}
+                                                        </div>
+                                                        <div className="product-card-price">
+                                                            <span><del>{formatCash(item.regular_price)} <sup>đ</sup></del></span>
+                                                            <span className="curr-price">{formatCash(item.sale_price)} <sup>đ</sup></span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        ))
+                                    }
+
+
+                                    <button aria-label="Previous" className="glider-prev">
+                                        <i className="ti-angle-double-left"></i>
+                                    </button>
+                                    <button aria-label="Next" className="glider-next">
+                                        <i className="ti-angle-double-right"></i>
+                                    </button>
+                                    <div role="tablist" className="dots"></div>
                                 </div>
-
                             </div>
-                        ))
-                    }
-                    {/*<div className="col-3 col-md-6 col-sm-12">*/}
-                    {/*    <div className="product-card">*/}
-                    {/*        <div className="product-card-img">*/}
-                    {/*            <img src="./images/product1_1.jpg" alt=""/>*/}
-                    {/*                <img src="./images/product24.jpg" alt=""/>*/}
-                    {/*        </div>*/}
-                    {/*        <div className="product-card-info">*/}
-                    {/*            <div className="product-btn">*/}
-                    {/*                <a href="products.html" className="btn-flat btn-hover btn-shop-now">shop now</a>*/}
-                    {/*                <button className="btn-flat btn-hover btn-cart-add">*/}
-                    {/*                    <i className='ti-shopping-cart'></i>*/}
-                    {/*                </button>*/}
-                    {/*                <button className="btn-flat btn-hover btn-cart-add">*/}
-                    {/*                    <i className='ti-heart'></i>*/}
-                    {/*                </button>*/}
-                    {/*            </div>*/}
-                    {/*            <div className="product-card-name">*/}
-                    {/*                Tên sản phẩm*/}
-                    {/*            </div>*/}
-                    {/*            <div className="product-card-price">*/}
-                    {/*                <span><del>$300</del></span>*/}
-                    {/*                <span className="curr-price">$200</span>*/}
-                    {/*            </div>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-
+                        </div>
+                    </div>
                 </div>
 
                 <div className="section-footer">
@@ -84,6 +74,46 @@ const Promotion = (props) => {
             </div>
         </div>
     );
+
+    // File css
+    // <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1/glider.min.css">
+
+    // File js
+    // <script src="https://cdn.jsdelivr.net/npm/glider-js@1/glider.min.js"></script>
+
+    // Code js để chạy slider
+    // <script>
+    //     new Glider(document.querySelector('.glider'), {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     scrollLock: true,
+    //     dots: '#dots',
+    //     arrows: {
+    //     prev: '.glider-prev',
+    //     next: '.glider-next'
+    // },
+    //     responsive: [
+    // {
+    //     breakpoint: 775,
+    //     settings: {
+    //     slidesToShow: 'auto',
+    //     slidesToScroll: 'auto',
+    //     itemWidth: 150,
+    //     duration: 0.25
+    // }
+    // },
+    // {
+    //     breakpoint: 1024,
+    //     settings: {
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     itemWidth: 150,
+    //     duration: 0.25
+    // }
+    // }
+    //     ]
+    // });
+    // </script>
 }
 
 export default Promotion;
